6cb5f6c91cbcb3979497448bde4e22d35b9e092f
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2025-04-04 00:56:27 +0200

    Add clean setup

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config(
+	{ ignores: ['dist'] },
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			eslintConfigPrettier,
+		],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			prettier: prettier,
+			'simple-import-sort': simpleImportSort,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+			'simple-import-sort/imports': [
+				'error',
+				{
+					groups: [
+						// React and external packages
+						['^react', '^@?\\w'],
+						// Internal packages and components
+						['^@/'],
+						// Parent and sibling imports
+						['^\\.\\./.*', '^\\./?$', '^\\./?.*'],
+						// Style imports
+						['^.+\\.?(css|scss|sass)$'],
+					],
+				},
+			],
+			'simple-import-sort/exports': 'error',
+		},
+	},
+);
